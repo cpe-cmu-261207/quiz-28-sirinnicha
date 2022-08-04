6e5a6db95574c7e6d836daf881d71e8e9492fe1f
sirinnicha <sirinnicha.ta@gmail.com>
2022-08-04 14:12:28 +0700

    add

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-async function callApi() {
+const img = document.getElementById("img-profile");
+const name = document.getElementById("p-name");
+const address = document.getElementById("p-address");
+const email = document.getElementById("p-email");
+const btnGenerate = document.getElementById("btn-random");
+
+// async function callApi() {
+//   const resp = await axios.get("https://randomuser.me/api/");
+// }
+btnGenerate.onclick = async () => {
   const resp = await axios.get("https://randomuser.me/api/");
-}
+};
